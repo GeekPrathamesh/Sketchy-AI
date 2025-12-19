a38c0fb40360a3543b7c653121dec4653e057ab0
GeekPrathamesh <prathameshteli727@gmail.com>
2025-12-20 03:24:10 +0530

    first commit - completed frontend base

---
+++ frontend/src/contexts/Appcontext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyChats, dummyUserData } from "../assets/assets";
+
+const Appcontext = createContext();
+
+export const useAppContext = () => useContext(Appcontext);
+
+export const AppcontextProvider = ({ children }) => {
+    const navigate = useNavigate();
+    const [user,setuser]=useState(null);
+    const [chats,setchats]=useState([]);
+    const [selectedchat,setselectedchat]=useState(null);
+    const [theme,settheme]=useState(localStorage.getItem("theme") || "light");
+
+    const fetchUser = ()=>{
+        setuser(dummyUserData);
+    };
+    const fetchUserchats=()=>{
+        setchats(dummyChats);
+        setselectedchat(dummyChats[0]);
+    }
+    useEffect(()=>{
+        fetchUser();
+    },[])
+    useEffect(()=>{
+        if(theme=="dark"){
+            document.documentElement.classList.add("dark");
+        }
+        else{
+            document.documentElement.classList.remove("dark");
+           
+        }
+        localStorage.setItem("theme",theme);
+    },[theme])
+    useEffect(()=>{
+        if(user){
+            fetchUserchats();
+        }
+        else{
+            setchats([]);
+            setselectedchat(null)
+        }
+    },[user])
+  let value = {navigate,user,setuser,chats,setchats,selectedchat,setselectedchat,theme,settheme};
+  return <Appcontext.Provider value={value}>{children}</Appcontext.Provider>;
+};
---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter} from "react-router-dom"
+import {AppcontextProvider} from "./contexts/Appcontext.jsx"
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+  <AppcontextProvider>
+  <StrictMode>
+    <App />
+  </StrictMode>
+  </AppcontextProvider>
+  </BrowserRouter>,
+)
